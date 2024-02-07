f9c79aa081b55efbe0109362d3a4992fc92e5af6
MiPigurim <michelepigurim26@gmail.com>
2024-02-07 13:03:37 -0300

    feat: separete app in components

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,6 @@
 import logo from './assests/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
 
 export function App() {
  return (
@@ -11,35 +13,15 @@ export function App() {
     <div className=' h-px bg-slate-700'></div>
 
     <div className=" grid grid-cols-3 gap-6 auto-rows-[250px]">
-      <div className=' rounded-md bg-slate-700 p-5 space-y-3'>
-        <span className=' text-sm font-medium text-slate-200'>Adicionar nota</span>
-        <p className=' text-sm leading-6 text-slate-400'>Grave uma nota em áudio que será convertida para texto automaticamente.</p>
-      </div>
 
+      <NewNoteCard />
 
-      <div className=' rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
-        <span className=' text-sm font-medium text-slate-300'>Há 2 dias</span>
-        <p className=' text-sm leading-6 text-slate-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempore numquam molestias perferendis, obcaecati, totam delectus necessitatibus ut ad nisi voluptatem, officia iure tempora omnis in architecto! Fuga, maxime facilis.</p>
+      <NoteCard />
+      <NoteCard />  
+      <NoteCard />  
+      <NoteCard />  
+      <NoteCard />  
 
-        <div className=' absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'/>
-      </div>
-
-      <div className=' rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
-        <span className=' text-sm font-medium text-slate-300'>Há 4 dias</span>
-        <p className=' text-sm leading-6 text-slate-400'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem expedita magni dicta, placeat odit quo commodi molestias, minus distinctio, atque provident aliquam. Mollitia illo maiores animi quaerat inventore explicabo distinctio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem expedita magni dicta, placeat odit quo commodi molestias, minus distinctio, atque provident aliquam. Mollitia illo maiores animi quaerat inventore explicabo distinctio.
-        </p>
-
-        <div className=' absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'/>
-      </div>
-
-      <div className=' rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
-        <span className=' text-sm font-medium text-slate-300'>Há 4 dias</span>
-        <p className=' text-sm leading-6 text-slate-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem expedita magni dicta, placeat odit quo commodi molestias, minus distinctio, atque provident aliquam. Mollitia illo maiores animi quaerat inventore explicabo distinctio.</p>
-        
-        <div className=' absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'/>
-      </div>
 
     </div>
   </div>
