254642946e971376b5699239be26db0d275692cd
MiPigurim <michelepigurim26@gmail.com>
2024-02-07 12:33:54 -0300

    feat: input 'busque suas notas'

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,14 @@ import logo from './assests/logo-nlw-expert.svg'
 
 export function App() {
  return (
-  <img src={logo} alt="" />
+  <div className=' mx-auto max-w-6xl my-12 space-y-6'>
+    <img src={logo} alt="NLW Expert" />
+    <form className=' w-full'>
+      <input type="text" placeholder='Busque em suas notas...' className=' w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'/>
+    </form>
+
+    <div className=' h-px bg-slate-700'></div>
+  </div>
     )
 }
 
