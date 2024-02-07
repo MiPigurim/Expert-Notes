cdf96df1ff3d5e24aaf79c72fd0838ff735b013b
MiPigurim <michelepigurim26@gmail.com>
2024-02-07 12:18:14 -0300

    feat: add logo in app

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
+import logo from './assests/logo-nlw-expert.svg'
+
 export function App() {
  return (
- <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <img src={logo} alt="" />
     )
 }
 
