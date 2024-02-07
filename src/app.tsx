54bfeacfe8fd0dea053dc43c61619833ae2525ec
MiPigurim <michelepigurim26@gmail.com>
2024-02-07 12:54:47 -0300

    feat: creat grid of notes

--- src/app.tsx
+++ src/app.tsx
@@ -9,6 +9,39 @@ export function App() {
     </form>
 
     <div className=' h-px bg-slate-700'></div>
+
+    <div className=" grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className=' rounded-md bg-slate-700 p-5 space-y-3'>
+        <span className=' text-sm font-medium text-slate-200'>Adicionar nota</span>
+        <p className=' text-sm leading-6 text-slate-400'>Grave uma nota em áudio que será convertida para texto automaticamente.</p>
+      </div>
+
+
+      <div className=' rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
+        <span className=' text-sm font-medium text-slate-300'>Há 2 dias</span>
+        <p className=' text-sm leading-6 text-slate-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempore numquam molestias perferendis, obcaecati, totam delectus necessitatibus ut ad nisi voluptatem, officia iure tempora omnis in architecto! Fuga, maxime facilis.</p>
+
+        <div className=' absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'/>
+      </div>
+
+      <div className=' rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
+        <span className=' text-sm font-medium text-slate-300'>Há 4 dias</span>
+        <p className=' text-sm leading-6 text-slate-400'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem expedita magni dicta, placeat odit quo commodi molestias, minus distinctio, atque provident aliquam. Mollitia illo maiores animi quaerat inventore explicabo distinctio.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem expedita magni dicta, placeat odit quo commodi molestias, minus distinctio, atque provident aliquam. Mollitia illo maiores animi quaerat inventore explicabo distinctio.
+        </p>
+
+        <div className=' absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'/>
+      </div>
+
+      <div className=' rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
+        <span className=' text-sm font-medium text-slate-300'>Há 4 dias</span>
+        <p className=' text-sm leading-6 text-slate-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem expedita magni dicta, placeat odit quo commodi molestias, minus distinctio, atque provident aliquam. Mollitia illo maiores animi quaerat inventore explicabo distinctio.</p>
+        
+        <div className=' absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'/>
+      </div>
+
+    </div>
   </div>
     )
 }
