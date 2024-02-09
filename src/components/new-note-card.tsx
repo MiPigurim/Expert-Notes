2c71ea13b2794ed76861cb720a3487e412efbd6b
MiPigurim <michelepigurim26@gmail.com>
2024-02-09 15:19:43 -0300

    feat: start and stop record

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -10,6 +10,7 @@ interface NewNoteCardProps {
 
 export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
+  const [isRecording, setIsRecording] = useState(false)
   const [content, setContent] = useState('' as string)
 
   function handleStartEditor() {
@@ -36,6 +37,14 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
     
   }
 
+  function handleStartRecording() {
+    setIsRecording(true)
+  }
+
+  function handleStopRecording() {
+    setIsRecording(false)
+  }
+
 
   return (
     <Dialog.Root>
@@ -51,7 +60,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
             <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
               <X className='size-5' />
             </Dialog.Close>
-              <form onSubmit={handleSaveNote} className='flex-1 flex flex-col'>
+              <form className='flex-1 flex flex-col'>
                 <div className='flex flex-1 flex-col gap-3 p-5'>
                   <span className=' text-sm font-medium text-slate-300'>
                     Adicionar nota        
@@ -59,16 +68,25 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
                   {shouldShowOnboarding ? (
                     <p className=' text-sm leading-6 text-slate-400'>
-                    Comece <button className='font-medium text-lime-400 hover:underline'>gravando uma nota</button> em áudio ou se preferir <button onClick={ handleStartEditor } className='font-medium text-lime-400 hover:underline'>utilize apenas texto</button>.                         
+                    Comece <button type='button' onClick={handleStartRecording} className='font-medium text-lime-400 hover:underline'>gravando uma nota</button> em áudio ou se preferir <button type='button' onClick={ handleStartEditor } className='font-medium text-lime-400 hover:underline'>utilize apenas texto</button>.                         
                     </p>
                   ) : (
                     <textarea autoFocus className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none' onChange={handleContentChanged} value={content}></textarea>
                   )}
                 </div>
 
-                <button type='submit' className='w-full bg-lime-400 py-4 text-center text-sm taxt-lime-950 outline-none font-medium hover:bg-lime-500'>
-                  Salvar nota
-                </button>
+                {isRecording ? (
+                  <button type='button' onClick={handleStopRecording} className='w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm taxt-slate-300 outline-none font-medium hover:slate-100'>
+                    <div className='size-3 rounded-full bg-red-500 animate-pulse'></div>
+                    Gravando! (clique para interromper)
+                  </button>
+                ) : (
+                  <button type='button' onClick={handleSaveNote} className='w-full bg-lime-400 py-4 text-center text-sm taxt-lime-950 outline-none font-medium hover:bg-lime-500'>
+                    Salvar nota
+                  </button>
+                )
+                 }
+
               </form>
             </Dialog.Content>
       </Dialog.Portal>
