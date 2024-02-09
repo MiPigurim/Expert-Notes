b80d32ba5152e50235d99a496a71b9dee83437bf
MiPigurim <michelepigurim26@gmail.com>
2024-02-09 15:01:10 -0300

    feat: filtered search notes

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import logo from './assests/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
@@ -10,13 +10,14 @@ interface Note {
 }
 
 export function App() {
+  const [search, setSearch] = useState('')
   const [notes, setNotes] = useState<Note[]>(() => {
     const notesOnStorage = localStorage.getItem('notes')
 
     if (notesOnStorage) {
       return JSON.parse(notesOnStorage)
     }
-    
+
     return[]
   })
 
@@ -34,11 +35,26 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value.toLowerCase()
+    setSearch(query)
+  }
+
+  const filteredNotes = search !== ''
+  ? notes.filter(note => note.content.toLowerCase().includes(search.toLowerCase()))
+  : notes
+  
+
  return (
   <div className=' mx-auto max-w-6xl my-12 space-y-6'>
     <img src={logo} alt="NLW Expert" />
     <form className=' w-full'>
-      <input type="text" placeholder='Busque em suas notas...' className=' w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'/>
+      <input 
+        type="text" 
+        placeholder='Busque em suas notas...' 
+        className=' w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+        onChange={handleSearch}
+      />
     </form>
 
     <div className=' h-px bg-slate-700'></div>
@@ -47,7 +63,7 @@ export function App() {
 
       <NewNoteCard onNoteCreated={onNoteCreated} />
 
-      {notes.map(note => {
+      {filteredNotes.map(note => {
         return <NoteCard key={note.id} note={note} />
       })}
        
